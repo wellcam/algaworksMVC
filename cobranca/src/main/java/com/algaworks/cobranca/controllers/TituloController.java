293c736baa2c6e09c9ce34a001d821c47412822b
Wellington Camilo <wel.camilo10@gmail.com>
2020-04-15 11:55:57 -0300

    Ajustando form

--- cobranca/src/main/java/com/algaworks/cobranca/controllers/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controllers/TituloController.java
@@ -1,13 +1,26 @@
 package com.algaworks.cobranca.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.algaworks.cobranca.model.Titulo;
+
 @Controller
+@RequestMapping("/titulo")
 public class TituloController {
 
-	@RequestMapping("/titulo/cadastro")
+	@GetMapping("/novo")
 	public String titulo() {
+		return "CadastroTitulo";
+	}
+	
+	@PostMapping
+	public String salvar(Titulo titulo) {
+		
+		System.out.println("OK");
+		
 		return "CadastroTitulo";
 	}
 }
